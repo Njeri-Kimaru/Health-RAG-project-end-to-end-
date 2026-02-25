0c6393d22d216fc43eaadad97809a8e7d007c065
Njeri-Kimaru <kimarumercy01@gmail.com>
2026-02-25 22:00:54 +0300

    new commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from rag_service import ask
+
+app = FastAPI(title = "Mental Health RAG API")
+
+class QuestionRequest(BaseModel):
+    question: str
+
+class QuestionResponse(BaseModel):
+    answer: str
+    sources: list
+
+@app.get("/") 
+def root():
+    return{"message":"RAG API is running"}
+
+@app.post("/ask", response_model=QuestionResponse)
+def ask_question(request: QuestionRequest):
+    try:
+        answer, sources = ask(request.question)
+
+        return QuestionResponse(
+            answer=answer,
+            sources=sources
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
